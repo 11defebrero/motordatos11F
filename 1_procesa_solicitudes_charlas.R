665afebba1f128783bbed7d3a9d9d8d3e5577fb9
María Medina <mariamedp@gmail.com>
2020-10-17 11:13:00 +0200

    Añadir CP en solicitudes corregidas

--- 1_procesa_solicitudes_charlas.R
+++ 1_procesa_solicitudes_charlas.R
@@ -10,13 +10,35 @@ ID_SHEET_SOLICITUDES_ORIGINAL <- config$ids_googledrive$solicitudes$original
 ID_SHEET_SOLICITUDES_LIMPIO <- config$ids_googledrive$solicitudes$limpio
 
 
-# Filtrar nuevas solicitudes
+# Cargar datos
 
 solicitudes_original <- get_solicitudes_charlas_original(file_id=ID_SHEET_SOLICITUDES_ORIGINAL)
 solicitudes_limpio <- get_solicitudes_charlas_limpio(file_id=ID_SHEET_SOLICITUDES_LIMPIO)
 
+
+# Filtrar nuevas solicitudes
+
 solicitudes_new <- solicitudes_original %>%
   filter(! id %in% solicitudes_limpio$id)
+
+
+# Añadir solicitudes corregidas que no tienen coordenadas para que se procesen también
+
+solicitudes_corregidas <- solicitudes_limpio %>%
+  filter(procesado == "CORREGIDO") %>%
+  filter(is.na(lon) | is.na(lat)) %>%
+  select(-lon, -lat) %>%
+  mutate(
+    fallos = "",
+    fallos_geolocalizacion = ""
+  )
+
+solicitudes_new <- solicitudes_new %>%
+  rbind(solicitudes_corregidas)
+
+solicitudes_limpio <- solicitudes_limpio %>%
+  filter(! id %in% solicitudes_corregidas$id)
+
 
 if (nrow(solicitudes_new) == 0) {
   stop("No hay nuevas solicitudes de actividad.")
@@ -43,6 +65,14 @@ solicitudes_new <- geolocaliza_solicitudes_charlas(solicitudes_new) %>%
 solicitudes_limpio <- solicitudes_limpio %>%
   rbind(solicitudes_new) %>%
   marca_duplicados_solicitudes_charlas()
+
+
+# Restaurar estado en las charlas corregidas
+
+solicitudes_limpio <- solicitudes_limpio %>%
+  mutate(
+    procesado = ifelse(id %in% solicitudes_corregidas$id, "CORREGIDO", procesado)
+  )
 
 
 # Subir a Google Drive
