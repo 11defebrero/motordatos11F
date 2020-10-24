50a5e972921fc3801ad89aebe763679b6a06da1a
popiula <anacream@hotmail.com>
2020-10-24 22:43:47 +0200

    Preparar EDA automático del paso 2

--- 99_genera_pdf_eda.R
+++ 99_genera_pdf_eda.R
@@ -10,6 +10,8 @@ DIR_EDA_LOCAL <- file.path(rprojroot::find_rstudio_root_file(), "data", "eda")
 DIR_EDA_DRIVE <- config$ids_googledrive$carpeta_eda
 
 ID_SHEET_SOLICITUDES_LIMPIO <- config$ids_googledrive$solicitudes$limpio
+ID_SHEET_CONTACTOS_LIMPIO <- config$ids_googledrive$contactos$limpio
+ID_SHEET_CONTACTOS_ENVIADO <- config$ids_googledrive$contactos$enviado
 
 
 ## Solicitudes charlas
@@ -24,3 +26,15 @@ rmarkdown::render(
 
 upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_solicitadas.pdf"), DIR_EDA_DRIVE)
 
+## Contactos con los centros para concertar una charla
+
+rmarkdown::render(
+  input = "templates/eda/resumen_contactos.Rmd",
+  params = list(id_sheet_limpios = ID_SHEET_CONTACTOS_LIMPIO, 
+                id_sheet_enviado = ID_SHEET_CONTACTOS_ENVIADO),
+  output_file = "resumen_contactos.pdf",
+  output_dir = DIR_EDA_LOCAL,
+  quiet = TRUE
+)
+
+upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_contactos.pdf"), DIR_EDA_DRIVE)
