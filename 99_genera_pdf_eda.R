15d6ce2b7cd6e26b8ba23ef9aed769e9b56ff82a
popiula <anacream@hotmail.com>
2020-11-13 22:22:56 +0100

    activar eda concertadas

--- 99_genera_pdf_eda.R
+++ 99_genera_pdf_eda.R
@@ -10,11 +10,15 @@ DIR_EDA_LOCAL <- file.path(rprojroot::find_rstudio_root_file(), "data", "eda")
 DIR_EDA_DRIVE <- config$ids_googledrive$carpeta_eda
 
 ID_SHEET_SOLICITUDES_LIMPIO <- config$ids_googledrive$solicitudes$limpio
+ID_SHEET_SOLICITUDES_RESTANTES <- config$ids_googledrive$solicitudes$restantes
+
 ID_SHEET_CONTACTOS_LIMPIO <- config$ids_googledrive$contactos$limpio
 ID_SHEET_CONTACTOS_ENVIADO <- config$ids_googledrive$contactos$enviado
 
+ID_SHEET_CONCERTADAS_LIMPIO <- config$ids_googledrive$concertadas$limpio
 
-## Solicitudes charlas
+
+# Solicitudes charlas
 
 rmarkdown::render(
   input = "templates/eda/resumen_solicitadas.Rmd",
@@ -26,15 +30,30 @@ rmarkdown::render(
 
 upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_solicitadas.pdf"), DIR_EDA_DRIVE)
 
-## Contactos con los centros para concertar una charla
 
-# rmarkdown::render(
-#   input = "templates/eda/resumen_contactos.Rmd",
-#   params = list(id_sheet_limpios = ID_SHEET_CONTACTOS_LIMPIO, 
-#                 id_sheet_enviado = ID_SHEET_CONTACTOS_ENVIADO),
-#   output_file = "resumen_contactos.pdf",
-#   output_dir = DIR_EDA_LOCAL,
-#   quiet = TRUE
-# )
-# 
-# upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_contactos.pdf"), DIR_EDA_DRIVE)
+# Contactos con los centros para concertar una charla
+
+ rmarkdown::render(
+   input = "templates/eda/resumen_contactos.Rmd",
+   params = list(id_sheet_limpios = ID_SHEET_CONTACTOS_LIMPIO, 
+                 id_sheet_enviado = ID_SHEET_CONTACTOS_ENVIADO),
+   output_file = "resumen_contactos.pdf",
+   output_dir = DIR_EDA_LOCAL,
+   quiet = TRUE
+ )
+ 
+upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_contactos.pdf"), DIR_EDA_DRIVE)
+
+
+# Charlas concertadas
+
+rmarkdown::render(
+  input = "templates/eda/resumen_concertadas.Rmd",
+  params = list(id_sheet = ID_SHEET_CONCERTADAS_LIMPIO),
+  output_file = "resumen_concertadas.pdf",
+  output_dir = DIR_EDA_LOCAL,
+  quiet = TRUE
+)
+
+upload_file_to_drive(file.path(DIR_EDA_LOCAL, "resumen_concertadas.pdf"), DIR_EDA_DRIVE)
+
