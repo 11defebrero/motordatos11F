5fade0e5a9b94595f10e2ca6682cb866c068a22c
Popiula <r.popiula@gmail.com>
2021-11-11 20:55:34 +0100

    activo scripts para la siguiente fase

--- 0_makefile.R
+++ 0_makefile.R
@@ -13,27 +13,27 @@ googledrive::drive_auth()
 
 # Charlas solicitadas
 
-source("1_procesa_solicitudes_charlas.R", encoding = "UTF-8")
+# source("1_procesa_solicitudes_charlas.R", encoding = "UTF-8")
 
 
 # Peticiones de contacto con centros
 
-# source("2a_procesa_contactos_centros.R", encoding = "UTF-8")
-# source("2b_envia_emails_contacto.R", encoding = "UTF-8")
+source("2a_procesa_contactos_centros.R", encoding = "UTF-8")
+source("2b_envia_emails_contacto.R", encoding = "UTF-8")
 
 
 # Charlas concertadas
 
-# source("3a_procesa_charlas_concertadas.R", encoding = "UTF-8")
-# source("3b_actualiza_solicitudes_charlas_restantes.R", encoding = "UTF-8")
+source("3a_procesa_charlas_concertadas.R", encoding = "UTF-8")
+source("3b_actualiza_solicitudes_charlas_restantes.R", encoding = "UTF-8")
 
 
 # Actividades
 
-# source("4a_procesa_actividades.R", encoding = "UTF-8")
-# source("4b_publica_actividades_wordpress.R", encoding = "UTF-8")
-# source("4c_revisa_actividades.R", encoding = "UTF-8")
-# source("4d_actualiza_url_wordpress.R", encoding = "UTF-8")
+source("4a_procesa_actividades.R", encoding = "UTF-8")
+source("4b_publica_actividades_wordpress.R", encoding = "UTF-8")
+source("4c_revisa_actividades.R", encoding = "UTF-8")
+source("4d_actualiza_url_wordpress.R", encoding = "UTF-8")
 
 
 # Actualización web
